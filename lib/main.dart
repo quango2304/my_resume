22a4f9ee03c625f952e80530421f453f837b069f
hiLeo <quanngovan98@gmail.com>
2021-05-16 22:38:45 +0700

    add url_strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:my_resume/screens/home_screen/home_screen_responsive.dart';
 import 'package:my_resume/utils/load_cv_json.dart';
-
-import 'constants/app_colors.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'screens/home_screen/home_screen_responsive.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
   await loadCV();
   runApp(MaterialApp(
     theme: ThemeData(
