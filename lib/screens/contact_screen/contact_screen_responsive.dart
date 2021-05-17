f550e597cb7c8844fe39526ff3fefec950bc9b81
Leo <quanngovan98@gmail.com>
2021-05-17 16:17:09 +0700

    refactor

--- lib/screens/contact_screen/contact_screen_responsive.dart
+++ lib/screens/contact_screen/contact_screen_responsive.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:my_resume/screens/contact_screen/contact_screen_desktop.dart';
 import 'package:my_resume/screens/contact_screen/contact_screen_mobile.dart';
+import 'package:my_resume/widgets/app_bar/app_bar_full_screen.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class ContactScreenResponsive extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    AppBarFullScreen.currentIndex = 3;
     return ScreenTypeLayout.builder(
       mobile: (_) => ContactScreenMobile(),
       tablet: (_) => ContactScreenDesktop(),
       desktop: (_) => ContactScreenDesktop(),
-      watch: (_) => ContactScreenMobile(),
+      watch: (_) => ContactScreenMobile(),˚
     );
   }
 }
--- lib/screens/home_screen/home_screen_responsive.dart
+++ lib/screens/home_screen/home_screen_responsive.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_resume/screens/home_screen/home_screen_desktop.dart';
 import 'package:my_resume/screens/home_screen/home_screen_mobile.dart';
+import 'package:my_resume/widgets/app_bar/app_bar_full_screen.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class HomeScreenResponsive extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    AppBarFullScreen.currentIndex = 0;
     return ScreenTypeLayout.builder(
       mobile: (_) => HomeScreenMobile(),
       tablet: (_) => HomeScreenDesktop(),
